7fe4f3c953bcb6000174835bc4e1e360771005b5
anneramie <annemariepandino@gmail.com>
2025-03-05 12:51:22 +0800

    first commit

---
+++ Attendance/website/extensions.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+socketio = SocketIO()
+login_manager = LoginManager()
+
+
+ip_address = "192.168.1.33"
+
+
+def save_data(data):
+
+     try:
+          db.session.add(data)
+          db.session.commit()
+
+          return('success')
+     except:
+          db.session.rollback()
+          return('failed')
